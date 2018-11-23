722abb2ec6673b6edae1a76066bc43e92f5c8e16
Seamus Kennedy <31747877+SKennedy94@users.noreply.github.com>
2018-11-22 22:31:37 -0800

    Server now shows all items
    Implemented the file reading and now send all of the data to each client that connects.

--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -1,11 +1,56 @@
 #include <iostream>
 #include <WS2tcpip.h>
 #include <string>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
 #include "Item.h"
 
 #pragma comment (lib, "ws2_32.lib")
 
 using namespace std;
+
+vector<Item> loadStoreData()
+{
+	vector<Item> itemList;
+	ifstream file("input.txt");
+	if (file.is_open())
+	{
+		string line;    //line from file
+		string word;    //word from line
+		string subLine;    //subtr of line
+		string anItem[3];
+		string itemDesc;
+		int itemUnits, itemPrice;
+		while (getline(file, line)) {
+			subLine = line.substr(0, 4);
+			if (subLine == "Item") {    //if the word is Item_
+				int itemIndex = 0;
+				istringstream iss(line);
+				while (iss >> word) {
+					anItem[itemIndex] = word;
+					itemIndex++;
+				}
+				itemDesc = anItem[0];
+				itemUnits = atoi(anItem[1].c_str());
+				itemPrice = atoi(anItem[2].c_str());
+				Item newItem = Item(itemDesc, itemUnits, itemPrice);
+				itemList.push_back(newItem);
+			}
+		}
+
+		file.close();
+		return itemList;
+	}
+	else
+	{
+		cout << "Unable to open file";
+		exit;
+	}
+}
 
 void main()
 {
@@ -24,7 +69,7 @@ void main()
 	serverHint.sin_addr.S_un.S_addr = ADDR_ANY;
 	serverHint.sin_family = AF_INET;
 	serverHint.sin_port = htons(54000); // convert from little to big endian
-	
+
 	if (bind(in, (sockaddr*)&serverHint, sizeof(serverHint)) == SOCKET_ERROR)
 	{
 		cout << "Can't bind socket!" << WSAGetLastError() << endl;
@@ -34,14 +79,18 @@ void main()
 	sockaddr_in client;
 	int clientLength = sizeof(client);
 	ZeroMemory(&client, clientLength);
-	
+
 	char buf[1024];
 
 	SOCKET out = socket(AF_INET, SOCK_DGRAM, 0);
-	
+
 	//test sending parts of an item
-	Item item = Item("name", 10, 100);
-	string s = item.getDescription() + " " + to_string(item.getUnits()) + " " + to_string(item.getPrice());
+	vector<Item> items = loadStoreData();
+	vector<string> s;
+	for ( int i = 0;  i < items.size(); i++)
+	{
+		s.push_back(items[i].getDescription() + " " + to_string(items[i].getUnits()) + " " + to_string(items[i].getPrice()));
+	}
 
 	//enter a loop
 	while (true)
@@ -56,10 +105,13 @@ void main()
 		}
 		else
 		{
-			int sendOK = sendto(out, s.c_str(), s.size() + 1, 0, (sockaddr*)&client, sizeof(client));
-			if (sendOK == SOCKET_ERROR)
+			for (int i =0; i <items.size(); i++)
 			{
-				cout << "didnt work" << WSAGetLastError() << endl;
+				int sendOK = sendto(out, s[i].c_str(), s[i].size() + 1, 0, (sockaddr*)&client, sizeof(client));
+				if (sendOK == SOCKET_ERROR)
+				{
+					cout << "didnt work" << WSAGetLastError() << endl;
+				}
 			}
 		}
 		// display response 
